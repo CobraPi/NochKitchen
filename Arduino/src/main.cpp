b74d2374dc8d48421e99274ecc20da4bafdf495d
Joseph Orduna <jorduna@allendatagraph.com>
2021-07-08 00:39:35 -0400

    changed arduino pins to correct PWM pins

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
-#define RED_LED_PIN 1
-#define GREEN_LED_PIN 3
-#define BLUE_LED_PIN 7
+#define RED_LED_PIN 9
+#define GREEN_LED_PIN 10
+#define BLUE_LED_PIN 11
 
+int red = 0;
+int green = 0;
+int blue = 0;
+
+void connect_serial() {
+
+}
+
+void read_and_send_PWM() {
+
+}
 
 void setup() {
     Serial.begin(115200);
